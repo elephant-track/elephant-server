adc6f280afd7af635dab9648edc239c19f820146
Ko Sugawara <ko.sugawara@ens-lyon.fr>
2021-06-29 16:00:27 +0200

    Update config file organization
    - config files are organized

--- script/generate_seg_labels.sh
+++ script/generate_seg_labels.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
-# cd /workspace/datasets/ISBI2021/BF-C2DL-HSC/ && python /opt/elephant/script/generate_seg_labels.py .
-# cd /workspace/datasets/ISBI2021/BF-C2DL-MuSC/ && python /opt/elephant/script/generate_seg_labels.py .
-# cd /workspace/datasets/ISBI2021/DIC-C2DH-HeLa/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/DIC-C2DH-HeLa_sparse.json .
-# cd /workspace/datasets/ISBI2021/Fluo-C2DL-MSC/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/Fluo-C2DL-MSC_sparse.json .
-# cd /workspace/datasets/ISBI2021/Fluo-N2DH-GOWT1/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/Fluo-N2DH-GOWT1_sparse.json .
-# cd /workspace/datasets/ISBI2021/Fluo-N2DL-HeLa/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/Fluo-N2DL-HeLa_sparse.json .
-# cd /workspace/datasets/ISBI2021/PhC-C2DH-U373/ && python /opt/elephant/script/generate_seg_labels.py .
-# cd /workspace/datasets/ISBI2021/PhC-C2DL-PSC/ && python /opt/elephant/script/generate_seg_labels.py .
-# cd /workspace/datasets/ISBI2021/Fluo-C3DH-A549/ && python /opt/elephant/script/generate_seg_labels.py .
-# cd /workspace/datasets/ISBI2021/Fluo-C3DH-H157/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/Fluo-C3DH-H157_sparse.json .
-# cd /workspace/datasets/ISBI2021/Fluo-C3DL-MDA231/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/Fluo-C3DL-MDA231_sparse.json .
-# cd /workspace/datasets/ISBI2021/Fluo-N3DH-CE/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/Fluo-N3DH-CE_sparse.json .
-# cd /workspace/datasets/ISBI2021/Fluo-N3DH-CHO/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/Fluo-N3DH-CHO_sparse.json .
+# cd /workspace/datasets/ISBI2021/BF-C2DL-HSC/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/BF-C2DL-HSC_sparse.json .
+# cd /workspace/datasets/ISBI2021/BF-C2DL-MuSC/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/BF-C2DL-MuSC_sparse.json .
+# cd /workspace/datasets/ISBI2021/DIC-C2DH-HeLa/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/DIC-C2DH-HeLa_sparse.json .
+# cd /workspace/datasets/ISBI2021/Fluo-C2DL-MSC/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/Fluo-C2DL-MSC_sparse.json .
+# cd /workspace/datasets/ISBI2021/Fluo-N2DH-GOWT1/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/Fluo-N2DH-GOWT1_sparse.json .
+# cd /workspace/datasets/ISBI2021/Fluo-N2DL-HeLa/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/Fluo-N2DL-HeLa_sparse.json .
+# cd /workspace/datasets/ISBI2021/PhC-C2DH-U373/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/PhC-C2DH-U373_sparse.json .
+# cd /workspace/datasets/ISBI2021/PhC-C2DL-PSC/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/PhC-C2DL-PSC_sparse.json .
+# cd /workspace/datasets/ISBI2021/Fluo-C3DH-A549/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/Fluo-C3DH-A549_sparse.json .
+# cd /workspace/datasets/ISBI2021/Fluo-C3DH-H157/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/Fluo-C3DH-H157_sparse.json .
+# cd /workspace/datasets/ISBI2021/Fluo-C3DL-MDA231/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/Fluo-C3DL-MDA231_sparse.json .
+# cd /workspace/datasets/ISBI2021/Fluo-N3DH-CE/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/Fluo-N3DH-CE_sparse.json .
+# cd /workspace/datasets/ISBI2021/Fluo-N3DH-CHO/ && python /opt/elephant/script/generate_seg_labels.py --sparse /opt/elephant/script/generate_seg_labels_configs/Fluo-N3DH-CHO_sparse.json .

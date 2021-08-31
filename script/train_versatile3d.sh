83c2a038bfaa7c805fcef97f5e5317f145aecd35
Ko Sugawara <ko.sugawara@ens-lyon.fr>
2021-08-31 18:53:16 +0200

    Training scripts for versatile3d
    - 4 conditions

--- script/train_versatile3d.sh
+++ script/train_versatile3d.sh
@@ -1,2 +1,5 @@
 #! /usr/bin/env bash
-python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/base_train_seg_versatile3d.json seg /opt/elephant/script/train_configs/versatile3d.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/base_train_seg_versatile3d_001.json seg /opt/elephant/script/train_configs/versatile3d_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/base_train_seg_versatile3d_002.json seg /opt/elephant/script/train_configs/versatile3d_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/base_train_seg_versatile3d_003.json seg /opt/elephant/script/train_configs/versatile3d_003.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/base_train_seg_versatile3d_004.json seg /opt/elephant/script/train_configs/versatile3d_004.json

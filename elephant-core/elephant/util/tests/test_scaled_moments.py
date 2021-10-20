639a8ffde12f58e08c1235f82336467d3bc8ec76
Ko Sugawara <ko.sugawara@ens-lyon.fr>
2021-10-20 16:35:44 +0200

    Use appropriate data for testing scaled_moments_central

--- elephant-core/elephant/util/tests/test_scaled_moments.py
+++ elephant-core/elephant/util/tests/test_scaled_moments.py
@@ -26,39 +26,15 @@
 
 import numpy as np
 from numpy.testing import assert_allclose
-from skimage import data
 
-from elephant.util.scaled_moments import scaled_centroid
 from elephant.util.scaled_moments import scaled_moments_central
 
 
-def test_np_linspace():
-    img = data.cell()
-    centroid = scaled_centroid(img, scales=(2.0, 1.0))
-    scales = (2.0, 1.0)
-    expected = {'mean': [48405.98313954717, 8403.084265729778],
-                'std': [101553.02372674456, 17625.788452893714]}
-    for (dim, dim_length), scale, in zip(enumerate(img.shape), scales):
-        delta = np.linspace(0, scale * dim_length, dim_length,
-                            endpoint=False, dtype=float) - centroid[dim]
-        powers_of_delta = delta[:, np.newaxis] ** np.arange(2 + 1)
-        assert powers_of_delta.mean() == expected['mean'][dim]
-        assert powers_of_delta.std() == expected['std'][dim]
-
-
-def test_scaled_centroid():
-    img = data.cell()
-    centroid = scaled_centroid(img, scales=(2.0, 1.0))
-    expected = np.array([663.687213, 274.497211])
-    assert_allclose(centroid, expected)
-
-
 def test_scaled_moments_central():
-    img = data.cell()
-    mu = scaled_moments_central(img, scales=(2.0, 1.0), order=2)
-    expected = np.array([
-        [2.46697460e7,   2.38418579e-7,  6.19935538e11],
-        [8.94069672e-08, 1.63203515e09,  -1.45016251e12],
-        [3.56890541e12,  -5.73021830e12, 9.06283186e16]
-    ])
+    img = np.zeros((20, 20), dtype=np.double)
+    img[13:17, 13:17] = 1
+    mu = scaled_moments_central(img, (2.0, 1.0), order=2)
+    expected = np.array([[16.,   0.,  20.],
+                         [0.,   0.,   0.],
+                         [80.,   0., 100.]])
     assert_allclose(mu, expected)

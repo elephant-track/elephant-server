1a9470dd104962833d4ade749481af7f5acc5500
Ko Sugawara <ko.sugawara@ens-lyon.fr>
2022-03-29 14:04:32 +0200

    Remove unused import

--- script/eval.py
+++ script/eval.py
@@ -25,7 +25,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # ==============================================================================
 """Commandline interface for prediction and export using a config file."""
-import os
 
 from elephant.common import detect_spots
 from elephant.config import SegmentationEvalConfig

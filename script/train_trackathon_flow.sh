a38768113c936b14dee83962d0bcce4efc326307
Ko <ko.sugawara@riken.jp>
2024-08-09 15:14:28 +0900

    Add scripts and configs for training flow models for the Trackathon paper

---
+++ script/train_trackathon_flow.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+#01
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_01_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_01_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_01_003.json
+# 02
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_02_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_02_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_02_003.json
+# 03
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_03_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_03_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_03_003.json
+# 04
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_04_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_04_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_04_003.json
+# 05
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_05_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_05_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_05_003.json
+# 06
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_06_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_06_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_06_003.json
+# 07
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_07_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_07_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_07_003.json
+# 08
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_08_001.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_08_002.json
+python /opt/elephant/script/train.py --baseconfig /opt/elephant/script/train_configs/trackathon_flow/base_train_flow_trackathon.json flow /opt/elephant/script/train_configs/trackathon_flow/train_flow_trackathon_08_003.json

4574b7272d8489532cc0587fc05248b4207e3b71
Ko Sugawara <ko.sugawara@riken.jp>
2024-12-06 15:15:57 +0900

    Fix permissions

--- docker/tls-gen.sh
+++ docker/tls-gen.sh
@@ -7,4 +7,5 @@ cd tls-gen/basic
 make CN=$RABBITMQ_USER
 cp -a /tmp/tls-gen/basic/result/*.pem /etc/ssl/mycerts/
 chown -R $UWSGI_UID:$UWSGI_GID /etc/ssl/mycerts
+chmod -R 644 /etc/ssl/mycerts
 rm -rf /tmp/tls-gen

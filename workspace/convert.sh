d94f124e732b8a01d2dad15eb5cabae4171a114f
Ko Sugawara <ko.sugawara@riken.jp>
2025-06-06 14:51:04 +0900

    Add data conversion script

---
+++ workspace/convert.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li13_T4.h5 /workspace/datasets/Suppl_data_3_li13_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li15_T5.h5 /workspace/datasets/Suppl_data_3_li15_T5
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li16_T5.h5 /workspace/datasets/Suppl_data_3_li16_T5
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li24_T4.h5 /workspace/datasets/Suppl_data_3_li24_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li24_T5.h5 /workspace/datasets/Suppl_data_3_li24_T5
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li30_T4.h5 /workspace/datasets/Suppl_data_3_li30_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li34_T4.h5 /workspace/datasets/Suppl_data_3_li34_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li36-T5.h5 /workspace/datasets/Suppl_data_3_li36-T5
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li37_T4.h5 /workspace/datasets/Suppl_data_3_li37_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li40_T4.h5 /workspace/datasets/Suppl_data_3_li40_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li41_T4.h5 /workspace/datasets/Suppl_data_3_li41_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li41_T5.h5 /workspace/datasets/Suppl_data_3_li41_T5
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li42_T5.h5 /workspace/datasets/Suppl_data_3_li42_T5
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li44_T4.h5 /workspace/datasets/Suppl_data_3_li44_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li47_T4.h5 /workspace/datasets/Suppl_data_3_li47_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li48_T4.h5 /workspace/datasets/Suppl_data_3_li48_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li48_T5.h5 /workspace/datasets/Suppl_data_3_li48_T5
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li49_T4.h5 /workspace/datasets/Suppl_data_3_li49_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li51_T4.h5 /workspace/datasets/Suppl_data_3_li51_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li51_T5.h5 /workspace/datasets/Suppl_data_3_li51_T5
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li52_T4.h5 /workspace/datasets/Suppl_data_3_li52_T4
+python /opt/elephant/script/dataset_generator.py /workspace/hdf5/Suppl_data_3_li52_T5.h5 /workspace/datasets/Suppl_data_3_li52_T5
